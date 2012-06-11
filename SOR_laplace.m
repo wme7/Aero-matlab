deb50523280337411faf0fa8d5130564ee43b4ad
Manuel Diaz <manuel.ade@gmail.com>
2012-06-11 21:49:41 +0800

    SOR schemes completed!

--- SOR_laplace.m
+++ SOR_laplace.m
@@ -1,7 +1,7 @@
 %% Escalar Poisson Equation 
 % 
 % Routine for solving a PNP-MOSFET junction for the region:
-% ________________________
+%__________________________
 %|++++|    ---N---   |++++|
 %|+P++|              |+P++|
 %|++++|              |++++|
@@ -13,9 +13,9 @@
 
 %% Governing equation
 %
-% $$\frac{\partial}{\partial x}\left (k(x,y)\frac{\partial{\phi }}{\partial{x}}\right)+
-%   \frac{\partial}{\partial y}\left (k(x,y)\frac{\partial{\phi}}{\partial{y}}\right)=
-%   \tau (\hat{n}-\hat{n}_d)$$
+% $$\frac{\partial}{\partial x}\left(k(x,y)\frac{\partial{\phi }}{\partial{x}}\right)+
+%   \frac{\partial}{\partial y}\left(k(x,y)\frac{\partial{\phi}}{\partial{y}}\right)=
+%   0$$
 %
 % Where: 
 %
@@ -26,24 +26,19 @@
 clear, clc, %close all;
 
 %% IC's
-omega = 1.4;
+omega = 1.6;
 delta = 0.1;
 
-L  = 1.8; dx = delta; x = 1:dx:L; n  = 18;
-H  = 1.2; dy = delta; y = 1:dy:H; m  = 12;
-
-tau = 1;
-
-n_1 = 1; % initial doping for region 1 (silicon)
-n_2 = 3; % initial doping for region 2 (gate A)
-n_3 = 3; % initial doping for region 3 (gate B)
+L = 1.8; dx = delta; x = 1:dx:L; n  = 18;
+H = 1.2; dy = delta; y = 1:dy:H; m  = 12;
 
 %% Grid
-n_d = ones(m,n);            % Silicon
-n_d(1:m/4, 1:n/6 ) = 3;     % gate A
-n_d(1:m/4, 5*n/6+1:n) = 3;  % gate B
-phi = zeros(m,n);           % electric potential grid
-phi_tilde = zeros(m,n); phi_next = zeros(m,n);
+n_d = ones(m,n);            % initial doping value for silicon
+n_d(1:m/4, 1:n/6 ) = 3;     % initial doping value for gate 1
+n_d(1:m/4, 5*n/6+1:n) = 3;  % initial doping value for gate 2
+
+phi = zeros(m,n);           % Electric potential grid (our goal)
+phi_tilde = zeros(m,n); phi_next = zeros(m,n); % Arrays for SOR
 
 % x gates locations:
 x_g1 = 1:n/6;
@@ -63,22 +58,22 @@ x_gates = zeros(1,n);
 x_gates(x_g1) = 1; x_gates(x_gc) = 1; x_gates(x_g2) = 1;
 
 % Dirichlet:
-phi(1,x_g1) =  0.0;     % gate A
+phi(1,x_g1) =  0.0;     % gate 1
 phi(1,x_gc) = -0.8;     % central gate
-phi(1,x_g2) =  1.0;     % gate B
+phi(1,x_g2) =  1.0;     % gate 2
 % Neumann BC: Every where else!
+
+% we know this boundary values of next 'tilde' step from the begining
+phi_tilde(1,x_g1) =  0.0; % gate 1
+phi_tilde(1,x_gc) = -0.8; % central gate
+phi_tilde(1,x_g2) =  1.0; % gate 2
 
 %% Main Loop
 A = zeros(m,n); B = zeros(m,n); C = zeros(m,n); D = zeros(m,n);
-
-% we know the boundary values from the begining
-phi_tilde(1,x_g1) =  0.0; % gate A
-phi_tilde(1,x_gc) = -0.8; % central gate
-phi_tilde(1,x_g2) =  1.0; % gate B
-
 % r = 0.0001; iter = 0; r_iter = 1; 
 % while r_iter >= r
-for s=1:1100
+for s=1:500
+    %SOR: Computing phi_tilde loop:
     for i=2:m-1
         for j=2:n-1
             % Update the change in k varaible
@@ -98,7 +93,7 @@ for s=1:1100
                     A(i,j)*phi(i+1,j)+...
                     C(i,j)*phi_tilde(i-1,j)+...
                     D(i,j)*phi_tilde(i,j-1));
-            elseif i == 2 && x_gates(j)==1
+            elseif i == 2 && x_gates(j) == 0
                 % Matrix Upper row, for the regions with Neumann BC
                 phi_tilde(i,j)=...
                     1/(k(i+1,j)+3*k(i,j)+k(i,j+1)+k(i,j-1))*(...
@@ -118,16 +113,17 @@ for s=1:1100
                     B(i,j)*phi(i,j+1)+...
                     C(i,j)*phi_tilde(i-1,j)+...
                     D(i,j)*phi_tilde(i,j-1));
-                    %+(delta^2)/4*;
             end
         end
     end
-    phi_tilde(2:m-1,1) = phi_tilde(2:m-1, 2 ); % Left matrix column, Neumann BC
-    phi_tilde(2:m-1,n) = phi_tilde(2:m-1,n-1); % Right matrix column, Neumann BC
-    phi_tilde(1, x_s1) = phi_tilde(2, x_s1  ); % Upper regions matrix row regions, Neumann BC
+    % Update Neumann BC's
+    phi_tilde(2:m ,1 ) = phi_tilde(2:m , 2  ); % Left array column 
+    phi_tilde(2:m ,n ) = phi_tilde(2:m ,n-1 ); % Right array column
+    phi_tilde(1, x_s1) = phi_tilde(2, x_s1  ); % Upper regions rwo array 
     phi_tilde(1, x_s2) = phi_tilde(2, x_s2  );
-    phi_tilde(m,2:n-1) = phi_tilde(m-1,2:n-1); % Lower matrix column, Neumann BC
-    % OSR steps:
+    phi_tilde(m,2:n-1) = phi_tilde(m-1,2:n-1); % Lower arraw row
+    
+    % SOR: compute phi next using realation constant: omega
     phi_next = phi + omega*( phi_tilde - phi );
     r_iter = norm(phi_next-phi,2);
     phi = phi_next;
@@ -138,8 +134,7 @@ end
 %% Plot Figures
 figure
 contourf(phi)
-title(['SOR Method, h =',num2str(delta),', \omega =',num2str(omega)])
-%title(['SOR Method, h =',num2str(h),', \omega =',num2str(omega),', iterations: ',num2str(iter)])
+title(['SOR Method, h = ',num2str(delta),', \omega = ',num2str(omega),', iterations: ',num2str(s)])
 xlabel('x points')
 ylabel('y points')
 colormap hot
--- SOR_poisson.m
+++ SOR_poisson.m
@@ -1,4 +1,4 @@
-%% Escalar Poisson Equation rutine solver for
+%% Escalar Poisson Equation 
 % 
 % Routine for solving a PNP-MOSFET junction for the region:
 % ________________________
@@ -13,7 +13,8 @@
 
 %% Governing equation
 %
-% $$\frac{\partial^2 \phi}{\partial x^2}+\frac{\partial^2 phi}{\partial x^2}=
+% $$\frac{\partial}{\partial x}\left(k(x,y)\frac{\partial{\phi }}{\partial{x}}\right)+
+%   \frac{\partial}{\partial y}\left(k(x,y)\frac{\partial{\phi}}{\partial{y}}\right)=
 %   -\tau (\hat{n}-\hat{n}_d)$$
 %
 % Where: 
@@ -25,34 +26,33 @@
 clear, clc, %close all;
 
 %% IC's
-omega = 1.4;    % Relaxation constant
-delta = 0.1;    % using an uniform grid in x and y, h = delta
-tau = 1;        % any constant
+omega = 1.6;    % Relaxation constant
+delta = 0.1;    % using an uniform grid: delta = dx = dy 
+tau   = 1.0;    % any constant, tau=0: laplace eqn problem
 
-L = 18; dx = delta; x = 1:dx:L; n  = round(L/delta);
-H = 12; dy = delta; y = 1:dy:H; m  = round(H/delta);
+L = 1.8; dx = delta; x = 0.1:dx:L; n  = 18;
+H = 1.2; dy = delta; y = 0.1:dy:H; m  = 12;
 
 %% Grid
-n_1 = 1; % initial doping value for silicon
-n_2 = 3; % initial doping value for gate 1
-n_3 = 3; % initial doping value for gate 2
-
+n_d = ones(m,n);            % initial doping value for silicon
+n_d(1:m/4, 1:n/6 ) = 3;     % initial doping value for gate 1
+n_d(1:m/4, 5*n/6+1:n) = 3;  % initial doping value for gate 2
 n_i = zeros(m,n);           % array for silicon doping chaging in every iteration
-n_d = ones(m,n);            % initial silicon doping array
-n_d(1:m/4, 1:n/6 )   = n_2; % set values of n_d for gate 1
-n_d(1:m/4, 5*n/6+1:n)= n_3; % set values of n_d for gate 2
 
 phi = zeros(m,n);           % Electric potential grid (our goal)
 phi_tilde = zeros(m,n); phi_next = zeros(m,n); % Arrays for SOR
 
-% gates locations in x:
+% x gates locations:
 x_g1 = 1:n/6;
 x_gc = 2*n/6+1:4*n/6;
 x_g2 = 5*n/6+1:n;
 
-% silicon location at the surface in x:
+% x silicon location at the surface 
 x_s1 = 1*n/6+1:2*n/6;
 x_s2 = 4*n/6+1:5*n/6;
+
+%% Computing k(x,y) on the grid:
+k = ones(m,n); % Assuming all k values constants
 
 %% BC's
 % bolean mask for gates locations 
@@ -60,87 +60,114 @@ x_gates = zeros(1,n);
 x_gates(x_g1) = 1; x_gates(x_gc) = 1; x_gates(x_g2) = 1;
 
 % Dirichlet:
-phi(1,x_g1) =  0.0;     % gate A
+phi(1,x_g1) =  0.0;     % gate 1
 phi(1,x_gc) = -0.8;     % central gate
-phi(1,x_g2) =  1.0;     % gate B
+phi(1,x_g2) =  1.0;     % gate 2
 % Neumann BC: Every where else!
 
 % we know this boundary values of next 'tilde' step from the begining
-phi_tilde(1,x_g1) =  0.0; % gate A
+phi_tilde(1,x_g1) =  0.0; % gate 1
 phi_tilde(1,x_gc) = -0.8; % central gate
-phi_tilde(1,x_g2) =  1.0; % gate B
+phi_tilde(1,x_g2) =  1.0; % gate 2
+
+
+
+%% Movie Parameters
+sEnd  = 500; % iterations
+sPlot = 10; frames = sEnd/sPlot; counter = 0;
+figure(1)
+colordef white %black
 
 %% Main Loop
-
-%r = 0.0001; iter = 0; r_iter = 1; 
-%while r_iter >= r
-%n_i = n_d; % evaluate laplace formulation!
-
-for s = 1000 % for fixed number of iterations
-    % Using a sqrt func to increase n_i until it reaches 2
+for s = 1:sEnd
+        % Using a sqrt func to increase n_i until it reaches 2
     if s < 10
         n_i = n_d.*(2-1/s^2); 
-        %n_i = n_d.*(2-1/iter^2); 
     else
-        n_i = 2.*n_d;
+        n_i = 2*n_d;
     end
-    % SOR: Computing phi_tilde loop:
+    %SOR: Computing phi_tilde loop:
     for i=2:m-1
         for j=2:n-1
             if j == 2 % Matrix's left column, Neumann BC
                 phi_tilde(i,j) = 1/3*(...
                     phi(i+1,j)+...
                     phi(i,j+1)+...
-                    phi_tilde(i-1,j))...
-                    +1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
+                    phi_tilde(i-1,j))+...
+                    1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
             elseif j == n-1 % Matrix's right column, Neumann BC
                 phi_tilde(i,j) = 1/3*(...
                     phi(i+1,j)+...
                     phi_tilde(i-1,j)+...
-                    phi_tilde(i,j-1))...
-                    +1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
-            elseif i == 2 && x_gates(j)==1
+                    phi_tilde(i,j-1))+...
+                    1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
+            elseif i == 2 && x_gates(j) == 0
                 % Matrix Upper row, for the regions with Neumann BC
                 phi_tilde(i,j) = 1/3*(...
                     phi(i+1,j)+...
                     phi(i,j+1)+...
-                    phi_tilde(i,j-1))...
-                    +1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
+                    phi_tilde(i,j-1))+...
+                    1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
             elseif i == m-1 % Matrix Lower row, Neumann BC
                 phi_tilde(i,j) = 1/3*(...
                     phi(i,j+1)+...
                     phi_tilde(i-1,j)+...
-                    phi_tilde(i,j-1))...
-                    +1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
+                    phi_tilde(i,j-1))+...
+                    1/3*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
             else
                 phi_tilde(i,j) = 1/4*(...
                     phi(i+1,j)+...
                     phi(i,j+1)+...
                     phi_tilde(i-1,j)+...
-                    phi_tilde(i,j-1))...
-                    +1/4*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
+                    phi_tilde(i,j-1))+...
+                    1/4*(delta^2)*tau*(n_i(i,j)-n_d(i,j));
             end
         end
     end
-    % Update for Neumann BC's
-    phi_tilde(2:m , 1) = phi_tilde(2:m , 2  ); % Left array column
-    phi_tilde(2:m , n) = phi_tilde(2:m , n-1); % Right array column
-    phi_tilde(1, x_s1) = phi_tilde(2, x_s1  ); % Upper array regions 
+    % Update Neumann BC's
+    phi_tilde(2:m ,1 ) = phi_tilde(2:m , 2  ); % Left array column 
+    phi_tilde(2:m ,n ) = phi_tilde(2:m ,n-1 ); % Right array column
+    phi_tilde(1, x_s1) = phi_tilde(2, x_s1  ); % Upper regions rwo array 
     phi_tilde(1, x_s2) = phi_tilde(2, x_s2  );
-    phi_tilde(m,2:n-1) = phi_tilde(m-1,2:n-1); % Lower array column
+    phi_tilde(m,2:n-1) = phi_tilde(m-1,2:n-1); % Lower arraw row
     
     % SOR: compute phi next using realation constant: omega
     phi_next = phi + omega*( phi_tilde - phi );
-    %r_iter = norm(phi_next-phi);
+    r_iter = norm(phi_next-phi,2);
     phi = phi_next;
-    %iter = iter+1;
+        
+    % Visualization
+%     if mod(s,sPlot) == 1
+%         counter = counter + 1;
+%         contourf(phi)
+%         colormap hot
+%         colorbar('location','southoutside')
+%         M(count) = getframe;
+%     end
+    
+    % Animated gif file
+%     if mod(s,sPlot) == 1
+%         F = getframe;
+%         if count == 1
+%             [im,map] = rgb2ind(F.cdata,256,'nodither');
+%             im(1,1,1,sEnd/sPlot) = 0;
+%         end
+%         im(:,:,1,count) = rgb2ind(F.cdata,map,'nodither');
+%     end
+    
 end
-%fprintf('iterations: %4.1f \n',iter);
 
 %% Plot Figures
+figure(2)
 contourf(phi)
 title(['SOR Method, h = ',num2str(delta),', \omega = ',num2str(omega),', iterations: ',num2str(s)])
 xlabel('x points')
 ylabel('y points')
 colormap hot
 colorbar('location','southoutside')
+
+%% Make Movie
+% movie(M,1,10); % movie(M,n,fps)
+
+%% Export to Gif
+% imwrite(im,map,'SOR_poisson.gif','DelayTime',0,'LoopCount',3)
