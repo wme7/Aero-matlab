7e4d5dac8245a9cd6bedc16569f12a6c9ebb2572
Manuel Diaz <manuel.ade@gmail.com>
2012-06-20 17:40:21 +0800

    update on weno5

--- WENO5.m
+++ WENO5.m
@@ -1,18 +1,35 @@
-function u_next = WENO5(f,x,u)
-%% Weighted Essentially Non-Oscilatory 5th order Accuarate
-% WENO 5 implementation subroutine for computing fluxes:
-% To solve for a 1D scalar advection equation. Algorith based on:
+%function u_next = WENO5(f,x,u)
+%% Weighted Essentially Non-Oscilatory for 5th order Accuracy
+% WENO 5 implementation subroutine for computing numerical fluxes at the
+% right and left boundaries of the every cell, that is 
+% $v_{i+\frac{1}{2}}^{-}$ and % $v_{i-\frac{1}{2}}^{+}$. To solve for a 1D 
+% scalar advection equation. 
+%
+% This algorithm based on lectures notes of:
 %
 %   Chi-Wang Shu; High-Order ENO and WENO schemes for Computational
 %   Fluid Dynamics, High-Order Methods for Computational Physics.
 %   Springer 1999.
+%
+% The basic idea is the following: instead of using only one of ENO
+% stencils to form a reconstruction, one uses a convex combination of all
+% of them as:
+%
+% For the right boundary: $v_{i+\frac{1}{2}}^{-}=\sum_{r=0}^{k-1}\omega_{r}
+% v_{i+\frac{1}{2}}^{(r)}$
+%
+% For the left boundary: $v_{i-\frac{1}{2}}^{+}=\sum_{r=0}^{k-1}
+% \widetilde{\omega}_{r} v_{i-\frac{1}{2}}^{(r)}$
 
-lx = length(x);  v = f(x); 
 if lx == lenfth(u) 
     error('the grid and data do not have the same length');
 end
-k = 3;  epsilon = 1e-25;
 
+%% Parameters
+k = 3;          % Polinomial degree
+
+%% Constants
+epsilon = 1e-6; % to avoid any alpha's denominator to become zero.
 
 %% Compute the weights:
 for i = 1:lx
@@ -61,4 +78,25 @@ end
         otherwise
             error('only options: 1, 2 and 3');
     end
+    
+r   = [-1 0 1 2];
+j   = [ 1 2];
+c_rj = [11/6 -7/6  1/3; 1/3  5/6 -1/6; -1/6  5/6  1/3; 1/3 -7/6 11/6];
 
+%% Reconstruction:
+% Following the formulation of reconstuction polinomials for Right and Left
+% side; that is $v_{i+\frac{1}{2}}^{(r)}$ and $v_{i-\frac{1}{2}}^{(r)}$
+%
+% Polinomials are defined as: $v_{i+\frac{1}{2}}^{-}=\sum_{j=0}^{k-1}c_{rj}
+% \overline{v}_{i-r+j}$ and $v_{i-\frac{1}{2}}^{+}=\sum_{j=0}^{k-1}
+% \widetilde{c}_{rj} \overline{v}_{i-r+j}$
+
+% k = 1
+
+% k = 2
+
+% k = 3
+    % right    
+        vr(i) = c_rj(1,1)*v() + c_rj(1,2)*v() + c_rj(1,3)*v()
+    % left 
+   
