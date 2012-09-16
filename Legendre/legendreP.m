8e802bc32faeaf6b778e91c8d9e3153ee389f83a
Manuel Diaz <manuel.ade@gmail.com>
2012-09-16 21:20:11 +0800

    I found a big bug in LegendreP!

--- Legendre/legendreP.m
+++ Legendre/legendreP.m
@@ -2,6 +2,9 @@ function legP = legendreP(x,k)
 % compute the value of the legendre polinomial of degree 'k' por column
 % values 'x'
 
+%number of points
+p = k+1;
+
 switch k
     case {0} % when k = 0
         legP = ones(size(x));
@@ -10,8 +13,8 @@ switch k
     otherwise % k >= 2 
         legP0 = ones(size(x));
         legP1 = x;
-        for m = (2):(k)
-            legPX = ((2*m+1).*x.*legP1 - m.*legP0)/(m+1);
+        for m = 1:k-1
+            legPX = ((2*m+1)*x.*legP1 - m*legP0)./(m+1);
             legP0 = legP1;
             legP1 = legPX;
         end
