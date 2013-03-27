d7432caf522a42bde9b7302e1b120bbbb1e98d52
Manuel Diaz <manuel.ade@gmail.com>
2013-03-27 19:40:13 +0800

    Generalizing cut function for other cases

--- HyperTangent/Runme.m
+++ HyperTangent/Runme.m
@@ -4,22 +4,27 @@ clear all; close all; clc;
 
 %% Define domain and ranges for the buffer region
 x = 0:0.01:1; 
-a = 0.45; b = 0.55;
+a = 0.4; b = 0.6;
 
-% call hyperbolic cut function,
-h = hcutfunc(x,a,b);
+%% Choose cut function model
+cutmodel = 1; % {1} scaled tanh(x), {2} scaled cos(x), {3} linear model
 
-% Plot h in a figure
+%% Call cut function,
+h = hcutfunc(x,a,b,cutmodel);
+
+%% Plot h(x) in a figure
 figure(1)
 % plot shade
     x_shade = linspace(a,b,10);
-    y1 = zeros(size(x_shade));
-    y2 = 1.2*ones(size(x_shade));
-    shade = shadedplot(x_shade,y1,y2,[0.7 0.7 1]);
+    y1 = -0.2*ones(size(x_shade));
+    y2 =  1.2*ones(size(x_shade));
+    [shade,l1,l2] = shadedplot(x_shade,y1,y2,[0.7 0.7 1]);
     % [1 0.7 0.7]: red area
     % [0.7 0.7 1]: blue area
 % plot h
 hold on
-    plot(x,h,'-.r');
-    axis([0,1,0,1.2])
+    func = plot(x,h,'-.k','LineWidth',2);
+    legend([shade(2),func],{'Buffer region','Cut function h(x)'},2);
+    xlabel('x'); ylabel('h(x)');
+    axis([0,1,-0.2,1.2]);
 hold off
--- HyperTangent/hcutfunc.m
+++ HyperTangent/hcutfunc.m
@@ -1,15 +1,14 @@
-function h = hcutfunc(x,a,b)
+function h = hcutfunc(x,a,b,modelfunc)
 % Compute Cut function scaling Hyperbolic tangent to the desired range.
 % NOTE: 'tanh' is constrained to work in the local range of [-4,4] and
 % 'cos' is constrained to work in the local range of [pi,2*pi]. We refer 
 % to this local domain $\xi$.
 % coded by Manuel Diaz, NTU, IAM, 2013.03.27.
-modelfunc = 2;
 
 % Idenfity regions,
 h_Euler = (x<a);
-h_BGK = (x>=b);
-h_Buffer = (x>=a & x<b);
+h_BGK = (x>b);
+h_Buffer = (x>=a & x<=b);
 
 % Identify node Id for every region,
 x_id_Euler = find(h_Euler);
@@ -28,6 +27,8 @@ switch modelfunc
     case{2} % using cos(x) for xi = [pi,2*pi]
         xi = (2*pi-(pi))/(b-a)*(x_buffer - a) - pi;
         h_func = 1/2*(cos(xi)+1);
+    case{3} % Linear function
+        h_func = 1/(b-a)*(x_buffer - a) - 0;
 end
 % define cut function h in all region
 h(x_id_Euler) = 0;
