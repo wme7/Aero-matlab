3f0ea16f84677fbf71b58770240da1297d0e917d
Manuel Diaz <manuel.ade@gmail.com>
2013-03-27 18:42:46 +0800

    Upgrading hcutfunc

--- HyperTangent/hcutfunc.m
+++ HyperTangent/hcutfunc.m
@@ -1,8 +1,10 @@
 function h = hcutfunc(x,a,b)
 % Compute Cut function scaling Hyperbolic tangent to the desired range.
-% NOTE: tanh is constrained to work in the local range of [-4,4], we call
-% this local domain $\xi$.
+% NOTE: 'tanh' is constrained to work in the local range of [-4,4] and
+% 'cos' is constrained to work in the local range of [pi,2*pi]. We refer 
+% to this local domain $\xi$.
 % coded by Manuel Diaz, NTU, IAM, 2013.03.27.
+modelfunc = 2;
 
 % Idenfity regions,
 h_Euler = (x<a);
@@ -19,10 +21,15 @@ x_buffer = x(x_id_Buffer);
 a = x_buffer(1); b = x_buffer(end);
 
 % apply transformation
-xi = (4-(-4))/(b-a)*(x_buffer - a) - 4;
-h_tanh = 1/2*(tanh(xi)+1);
-
+switch modelfunc
+    case{1} % using tanh(x) for xi = [-4,4]
+        xi = (4-(-4))/(b-a)*(x_buffer - a) - 4;
+        h_func = 1/2*(tanh(xi)+1);
+    case{2} % using cos(x) for xi = [pi,2*pi]
+        xi = (2*pi-(pi))/(b-a)*(x_buffer - a) - pi;
+        h_func = 1/2*(cos(xi)+1);
+end
 % define cut function h in all region
 h(x_id_Euler) = 0;
-h(x_id_Buffer) = h_tanh(:);
+h(x_id_Buffer) = h_func(:);
 h(x_id_BGK) = 1;
